40f09aac88f2364568373b1ddb500cb9975fe556
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2022-11-17 11:01:08 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjetoUdemy/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjetoUdemy
+{
+    internal class Pessoa
+    {
+
+        public string Nome1;
+        public int Idade1;
+        public string Nome2;
+        public int Idade2;
+
+        public int CPF;
+        public int CPF2;
+
+        
+
+    }
+}
---
+++ PrimeiroProjetoUdemy/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Globalization;
+using System.Xml;
+
+
+namespace PrimeiroProjetoUdemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa x, y, z, p, cpf1, cpf2;
+            Times a, b, c, d;
+            
+
+            x = new Pessoa();
+            z = new Pessoa();
+
+            a = new Times();
+            b = new Times();
+            c = new Times();
+
+            cpf1 = new Pessoa();
+            cpf2 = new Pessoa();
+
+
+            x.Nome1 = "Pedrin";
+            z.Nome2 = "joaquina";
+
+            a.Nome = "Liqud";
+            a.Titulos = 50;
+            a.Color = "Azul";
+
+
+            b.Nome = "FaZe";
+            b.Titulos = 40;
+            b.Color = "Red";
+
+            c.Nome = "NiP";
+            c.Titulos = 35;
+            c.Color = "Green";
+
+            cpf1.CPF = 909090;
+            cpf2.CPF2 = 101010;
+
+            Console.WriteLine(a.Nome + " " + b.Nome + " e " + c.Nome  + " Esses times fazem parte do RainbowSix") ;
+            Console.WriteLine("O " + x.Nome1 + " disse que o time preferido dele é a " + a.Nome);
+            Console.WriteLine("A " + z.Nome2 + " disse que o time preferido dela é a " + b.Nome);
+            Console.WriteLine("Somando os títulos da " + a.Nome+  " e " + b.Nome + " Teremos o resultado " + (a.Titulos + b.Titulos));
+            Console.WriteLine("O CPF do " + x.Nome1 + " é " + cpf1.CPF + " e o da " + z.Nome2 + " é " + cpf2.CPF2);
+
+            
+             
+
+
+
+        }
+    }
+}
+
